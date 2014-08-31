22d4b4d7b53662630c70ffd7a3037ce0f52cce37
PetrifiedLasagna <socramthesamurai@yahoo.com>
2014-08-31 10:56:06 -0400

    Add MSVC bin

---
+++ default_workspace/Microsoft Visual C++ toolkit 2003/bin/vcvars32.bat
@@ -0,0 +1,19 @@
+@echo off
+
+Set PATH=C:\Program Files\Microsoft Visual C++ Toolkit 2003\bin;%PATH%
+Set INCLUDE=C:\Program Files\Microsoft Visual C++ Toolkit 2003\include;%INCLUDE%
+Set LIB=C:\Program Files\Microsoft Visual C++ Toolkit 2003\lib;%LIB%
+
+echo Setting environment for using Microsoft Visual C++ 2003 Toolkit.
+echo (If you have another version of Visual Studio or Visual C++ installed and wish
+echo to use its tools from the command line, run vcvars32.bat for that version.)
+echo.
+echo Thank you for choosing the Visual C++ Toolkit 2003!  Get started quickly by
+echo building the code samples included in the "Samples" directory.  Each sample
+echo includes a short whitepaper discussing the Visual C++ features, and a batch
+echo file for building the code.
+echo.
+echo Type "cl /?" for brief documentation on compiler options.
+echo.
+echo Visit http://msdn.microsoft.com/visualc/using/documentation/default.aspx for
+echo complete compiler documentation.
