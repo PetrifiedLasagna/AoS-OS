77dfcea17de477afbeeb4271173383609ff9d9a7
PetrifiedLasagna <socramthesamurai@yahoo.com>
2014-09-07 15:52:02 -0400

    organized directories into seperate branches

--- default_workspace/setup.bat
+++
@@ -1,9 +0,0 @@
-@rem make sure the paths point to the correct locations
-@rem you may also need to change vcvars32.bat to match the correct locations
-call "C:\Program Files\Microsoft Visual C++ Toolkit 2003\bin\vcvars32.bat"
-set INCLUDE=C:\Users\Administrator\Desktop\dxs\Include;%INCLUDE%
-set LIB=C:\Users\Administrator\Desktop\dxs\Lib\x86;%LIB%
-
-echo.
-echo environment setup complete
-echo.
