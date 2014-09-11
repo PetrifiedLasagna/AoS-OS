3b1a14b1bb6cf414ac35334bf721db7904dbba80
PetrifiedLasagna <socramthesamurai@yahoo.com>
2014-09-11 12:04:00 -0400

    added new includes: enet-1.3.12, zlib 1.2.1

--- setup.bat
+++ setup.bat
@@ -1,7 +1,5 @@
-@rem make sure the paths point to the correct locations
-@rem you may also need to change vcvars32.bat to match the correct locations
 call "C:\Program Files\Microsoft Visual C++ Toolkit 2003\bin\vcvars32.bat"
-set INCLUDE=C:\Users\Administrator\Desktop\dxs\Include;%INCLUDE%
+set INCLUDE=C:\Users\Administrator\Desktop\enet-1.3.12\include;C:\Users\Administrator\Desktop\dxs\Include;%INCLUDE%
 set LIB=C:\Users\Administrator\Desktop\dxs\Lib\x86;%LIB%
 
 echo.
