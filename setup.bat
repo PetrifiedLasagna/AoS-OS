66ae33112acf95d7aa7be4af8d3664f86c822a58
PetrifiedLasagna <socramthesamurai@yahoo.com>
2014-09-11 13:40:26 -0400

    fixed dependencies

--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
 call "C:\Program Files\Microsoft Visual C++ Toolkit 2003\bin\vcvars32.bat"
-set INCLUDE=C:\Users\Administrator\Desktop\enet-1.3.12\include;C:\Users\Administrator\Desktop\dxs\Include;%INCLUDE%
-set LIB=C:\Users\Administrator\Desktop\dxs\Lib\x86;%LIB%
+set INCLUDE=C:\Users\Administrator\Desktop\zlib-1.2.1\include;C:\Users\Administrator\Desktop\enet-1.3.12\include;C:\Users\Administrator\Desktop\dxs\Include;%INCLUDE%
+set LIB=C:\Users\Administrator\Desktop\enet-1.3.12\lib;C:\Users\Administrator\Desktop\zlib-1.2.1\lib;C:\Users\Administrator\Desktop\dxs\Lib\x86;%LIB%
 
 echo.
 echo environment setup complete
